c7e3e514ffaf164c8df2a8c8e19aa57a8612bc2f
Steven Troxler <steven.troxler@cloudlabs.io>
2015-06-30 19:19:46 -0700

    Initial commit: application-plugin example
    Also created resource-switching, but I haven't added the
    switching logic yet.

---
+++ application-plugin/src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+  @Test
+  public void testStupidStuff() {
+
+    // assert statements
+    assertEquals(true, true);
+  }
+
+} 
